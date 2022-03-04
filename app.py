0662f407e302cc38d8afa427a5b5ef952738e783
Мефодий <storage@list.ru>
2022-03-05 04:18:15 +1000

    Step 3, 4, 1*. Modified CBV for:     /movies/?director_id=1     /movies/?genre_id=1     /movies/?director_id=2&genre_id=4

--- app.py
+++ app.py
@@ -64,38 +64,77 @@ movie_ns = api.namespace('movies')
 @movie_ns.route('/')
 class MoviesView(Resource):
     def get(self):
+        # # # # # # # # # # # #
+        # movies = Movie.query.all()
+        # result = []
+        # for movie in movies:
+        #     dict = {
+        #         'id': movie.id,
+        #         'title': movie.title,
+        #         'description': movie.description,
+        #         'trailer': movie.trailer,
+        #         'year': movie.year,
+        #         'rating': movie.rating,
+        #         'genre_id': Genre.query.get(movie.genre_id).name,
+        #         'director_id': Director.query.get(movie.director_id).name
+        #         }
+        #     result.append(dict)
+        # return result, 200
+        # # # # # # # # # # # #
         movies = movies_schema.dump(Movie.query.all())
-        result = []
-        for movie in movies:
-            dict = {
-                'id': movie['id'],
-                'title': movie['title'],
-                'description': movie['description'],
-                'trailer': movie['trailer'],
-                'year': movie['year'],
-                'rating': movie['rating'],
-                'genre_id': movie['genre_id'],
-                'director_id': movie['director_id']
-                }
-            result.append(dict)
-        return result, 200
+        # result = []
+        # for movie in movies:
+        #     dict = {
+        #         'id': movie['id'],
+        #         'title': movie['title'],
+        #         'description': movie['description'],
+        #         'trailer': movie['trailer'],
+        #         'year': movie['year'],
+        #         'rating': movie['rating'],
+        #         'genre_id': movie['genre_id'],
+        #         'director_id': movie['director_id']
+        #         }
+        #     result.append(dict)
+
+        dir_id = request.args.get('director_id')
+        genre_id = request.args.get('genre_id')
+
+        if dir_id or genre_id:
+            for movie in movies:
+                if dir_id and genre_id:
+                    if dir_id == str(movie['director_id']) and genre_id == str(movie['genre_id']):
+                        return movie, 200
+                    else:
+                        return '', 404
+                elif dir_id:
+                    if dir_id == str(movie['director_id']):
+                        return movie, 200
+                    else:
+                        return '', 404
+                elif genre_id:
+                    if genre_id == str(movie['genre_id']):
+                        return movie, 200
+                    else:
+                        return '', 404
+
+        return movies, 200
 
 
 @movie_ns.route('/<int:mid>')
 class MoviesView(Resource):
     def get(self, mid):
         movie = movie_schema.dump(Movie.query.get(mid))
-        result = {
-            'id': movie['id'],
-            'title': movie['title'],
-            'description': movie['description'],
-            'trailer': movie['trailer'],
-            'year': movie['year'],
-            'rating': movie['rating'],
-            'genre_id': movie['genre_id'],
-            'director_id': movie['director_id']
-        }
-        return result, 200
+        # result = {
+        #     'id': movie['id'],
+        #     'title': movie['title'],
+        #     'description': movie['description'],
+        #     'trailer': movie['trailer'],
+        #     'year': movie['year'],
+        #     'rating': movie['rating'],
+        #     'genre_id': movie['genre_id'],
+        #     'director_id': movie['director_id']
+        # }
+        return movie, 200
 
 
 if __name__ == '__main__':
